e20d15e0e114592d64f6d91605d1d004af901278
KZJ <t108360138@ntut.org.tw>
2021-12-23 02:45:25 +0800

    first commit

---
+++ test_p10/src/pb/sample3.java
@@ -0,0 +1,11 @@
+package pb;
+
+class sample3 {
+	public static void main (String[] args)
+	{
+		pc.Car car1;
+		car1=new pc.Car();
+		car1.show();	
+	}
+
+}
---
+++ test_p34/src/sample11.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class sample11 {
+	public static void main (String[] args) 
+	{
+		System.out.println("請輸入字串");
+		try 
+		{
+            BufferedReader br =
+            		new BufferedReader(new InputStreamReader(System.in));
+			String str =br.readLine();
+			System.out.println("已經輸入字串"+str);
+				}
+		catch(IOException e) 
+		{
+			System.out.println("輸出入錯誤");
+			
+		}
+	}
+
+}
